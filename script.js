157c5160a0d6ba4f01c1c996878ca01424254b04
jworboys <Jordworb@gmail.com>
2021-05-03 19:35:29 -0400

    Adding winner background change

--- script.js
+++ script.js
@@ -8,18 +8,54 @@
 // document.querySelector('.guess').value = 23;
 
 const secretNumber = Math.trunc(Math.random()*20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
 
+    // No input
     if(!guess) {
         document.querySelector('.message').textContent = 'No number!!';
-    } else if (guess === secretNumber){
+    } 
+
+    // Player wins the game
+    else if (guess === secretNumber)
+    {
         document.querySelector('.message').textContent = 'Correct Number!!';
-    } else if (guess > secretNumber){
-        document.querySelector('.message').textContent = 'Too high!!';
-    } else if (guess < secretNumber){
-        document.querySelector('.message').textContent = 'Too low!!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+    } 
+
+
+    //Guess to high
+    else if (guess > secretNumber)
+    {
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too high!!';
+            score--;
+            document.querySelector('.score').textContent = score; 
+        }
+        else
+        {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0; 
+        }
+    } 
+
+
+    // Guess to low
+    else if (guess < secretNumber)
+    {
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too Low!!';
+            score--;
+            document.querySelector('.score').textContent = score; 
+        }
+        else
+        {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0; 
+        }
     }
 });
