5fc0ab2175a6a6bab396e3cf9c2e5f28c86f3d0d
jworboys <Jordworb@gmail.com>
2021-05-02 22:24:57 -0400

    Basic functionality

--- script.js
+++ script.js
@@ -7,7 +7,19 @@
 // console.log(document.querySelector('.guess').value); 
 // document.querySelector('.guess').value = 23;
 
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
-    console.log(document.querySelector('.guess').value);
+    const guess = Number(document.querySelector('.guess').value);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No number!!';
+    } else if (guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number!!';
+    } else if (guess > secretNumber){
+        document.querySelector('.message').textContent = 'Too high!!';
+    } else if (guess < secretNumber){
+        document.querySelector('.message').textContent = 'Too low!!';
+    }
 });
